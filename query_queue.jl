72d660c6154446de1d0c4cfad87df990ff78e5d5
Anthony Gagnon <59715170+dany-demise@users.noreply.github.com>
2024-10-13 22:53:26 -0400

    Create query_queue.jl

---
+++ query_queue.jl
@@ -0,0 +1,26 @@
+module AsyncQueue
+
+# Create an unbounded channel to represent the queue
+const queue = Channel{Any}()
+
+# Function to push an item onto the queue
+function push!(item)
+    put!(queue, item)
+end
+
+# Function to pop an item from the queue
+function pop!()
+    return take!(queue)
+end
+
+# Listener function to handle new messages as they arrive
+function start_listener(callback::Function)
+    @async begin
+        while true
+            item = take!(queue)  # Waits for a new item
+            callback(item)        # Call the callback function with the new item
+        end
+    end
+end
+
+end # module
